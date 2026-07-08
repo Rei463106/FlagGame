0d17f90ecfac490dc9f149fb01ffe32fd514b2b4
mayarei2662 <mayarei2662@gmail.com>
2026-07-08 13:56:46 +0900

    [Add]ふわふわ

---
+++ Assets/FlowCharacter.cs
@@ -0,0 +1,7 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class FlowCharacter : MonoBehaviour
+{
+    private void Awake() => transform.DOMoveY(transform.position.y + 0.3f, 1f) .SetLoops(-1, LoopType.Yoyo) .SetEase(Ease.InOutSine);
+}
