97099c6adae30faffb0fe31bf1da0593a1edc722
mayarei2662 <mayarei2662@gmail.com>
2026-07-09 10:19:19 +0900

    [Fix]浮く動きの修正

--- Assets/FlowCharacter.cs
+++ Assets/FlowCharacter.cs
@@ -3,5 +3,6 @@ using UnityEngine;
 
 public class FlowCharacter : MonoBehaviour
 {
-    private void Awake() => transform.DOMoveY(transform.position.y + 0.3f, 1f) .SetLoops(-1, LoopType.Yoyo) .SetEase(Ease.InOutSine);
+    private void Awake() => transform.DOMoveY(transform.position.y + 0.3f, 1f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+    private void OnDestroy() => transform.DOKill();
 }
